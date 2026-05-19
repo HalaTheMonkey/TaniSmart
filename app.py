789c6d47a8a365388094d2ca7f5ddcbe969a8c35
HalaTheMonkey <dindasalz73@gmail.com>
2026-05-19 23:31:54 +0700

    Update app.py
    Update sekarang bagian backend nya sudah nyabung bagian frontendnya yaitu bagian htmlnya

--- app.py
+++ app.py
@@ -1,33 +1,93 @@
 from flask import Flask
+from flask import render_template
 from flask import request
 from flask import jsonify
 
 app = Flask(__name__)
 
-# TEMPORARY STORAGE
+# =========================
+# TEMPORARY SENSOR STORAGE
+# =========================
+
 sensor_data = {
-    "ph": 0,
-    "temperature": 0,
-    "soil_moisture": 0,
-    "air_humidity": 0,
-    "light": 0
+    "ph": 6.5,
+    "temperature": 37,
+    "soil_moisture": 18,
+    "air_humidity": 62,
+    "light": 820
 }
 
-# ROUTE HOME
+# =========================
+# HOME PAGE
+# =========================
+
 @app.route("/")
 def home():
 
-    return f"""
-    <h1>Data Sensor</h1>
+    return render_template(
 
-    <p>pH: {sensor_data['ph']}</p>
-    <p>Suhu: {sensor_data['temperature']}</p>
-    <p>Kelembapan Tanah: {sensor_data['soil_moisture']}</p>
-    <p>Kelembapan Udara: {sensor_data['air_humidity']}</p>
-    <p>Cahaya: {sensor_data['light']}</p>
-    """
+        "home.html",
 
-# ROUTE MENERIMA DATA DARI ESP32
+        ph=sensor_data["ph"],
+        temperature=sensor_data["temperature"],
+        soil_moisture=sensor_data["soil_moisture"],
+        air_humidity=sensor_data["air_humidity"],
+        light=sensor_data["light"]
+
+    )
+
+# =========================
+# QUESTION PAGE
+# =========================
+
+@app.route("/question")
+def question():
+
+    return render_template("questions.html")
+
+# =========================
+# RECOMMENDATION YES PAGE
+# =========================
+
+@app.route("/recommendation-yes")
+def recommendation_yes():
+
+    return render_template(
+
+        "recommendation_yes.html",
+
+        ph=sensor_data["ph"],
+        temperature=sensor_data["temperature"],
+        soil_moisture=sensor_data["soil_moisture"],
+        air_humidity=sensor_data["air_humidity"],
+        light=sensor_data["light"]
+
+    )
+
+# =========================
+# RECOMMENDATION NO PAGE
+# =========================
+
+@app.route("/recommendation-no")
+def recommendation_no():
+
+    return render_template(
+
+        "Rekomendasi_no.html",
+
+        ph=sensor_data["ph"],
+        temperature=sensor_data["temperature"],
+        soil_moisture=sensor_data["soil_moisture"],
+        air_humidity=sensor_data["air_humidity"],
+        light=sensor_data["light"]
+
+    )
+
+# =========================
+# RECEIVE SENSOR DATA
+# FROM ESP32 / ARDUINO
+# =========================
+
 @app.route("/sensor-data", methods=["POST"])
 def receive_sensor_data():
 
@@ -45,7 +105,10 @@ def receive_sensor_data():
         "message": "Data received successfully"
     })
 
+# =========================
 # RUN FLASK
+# =========================
+
 if __name__ == "__main__":
 
     app.run(debug=True)
